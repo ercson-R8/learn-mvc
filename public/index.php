f17b8fed86ff3ba25ad26903f1b27633c46e7b9d
ericson billedo <ericson.billedo@upou.edu.h>
2016-12-27 09:17:18 +0400

    Section: 3 Advanced routing: add simpler but more powerful routes

--- public/index.php
+++ public/index.php
@@ -1,8 +1,36 @@
 <?php
 
-define ('DS', DIRECTORY_SEPARATOR);
-define ('ROOT', dirname(dirname(__FILE__)));
+/**
+ * Front controller
+ *
+ * PHP version 5.4
+ */
 
-$url = $_SERVER['REQUEST_URI'];
+/**
+ * Routing
+ */
+require '../Core/Router.php';
 
-print_r($url);
+$router = new Router();
+
+// Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+    
+// Display the routing table
+//echo '<pre>';
+//var_dump($router->getRoutes());
+//echo '</pre>';
+
+
+// Match the requested route
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)) {
+    echo '<pre>';
+    var_dump($router->getParams());
+    echo '</pre>';
+} else {
+    echo "No route found for URL '$url'";
+}
