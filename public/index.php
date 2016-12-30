ff88eff03c58ca1de4b0024c7ab5773322b24ff1
SATLYTE\ercson <ericson.billedo@upou.edu.ph>
2016-12-30 13:50:10 +0400

    lesson 32

--- public/index.php
+++ public/index.php
@@ -7,30 +7,26 @@
  */
 
 /**
+ * Autoloader
+ */
+spl_autoload_register(function ($class) {
+    $root = dirname(__DIR__);   // get the parent directory
+    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
+    if (is_readable($file)) {
+        require $root . '/' . str_replace('\\', '/', $class) . '.php';
+    }
+});
+
+
+/**
  * Routing
  */
-require '../Core/Router.php';
-
-$router = new Router();
+$router = new Core\Router();
 
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
-$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
-$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+$router->add('{controller}/{action}');
+$router->add('{controller}/{id:\d+}/{action}');
+$router->add('admin/{controller}/{action}', ['namespace' => 'Admin']);
     
-// Display the routing table
-//echo '<pre>';
-//var_dump($router->getRoutes());
-//echo '</pre>';
-
-
-// Match the requested route
-$url = $_SERVER['QUERY_STRING'];
-
-if ($router->match($url)) {
-    echo '<pre>';
-    var_dump($router->getParams());
-    echo '</pre>';
-} else {
-    echo "No route found for URL '$url'";
-}
+$router->dispatch($_SERVER['QUERY_STRING']);
