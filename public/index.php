975b976104e78de4a2b8d5bd0e627c765bc3cdca
SATLYTE\ercson <ericson.billedo@upou.edu.ph>
2017-01-04 16:56:12 +0400

    Complete MVC
    A generic MVC Framework

--- public/index.php
+++ public/index.php
@@ -19,6 +19,14 @@ Twig_Autoloader::register();
 
 
 /**
+ * Error and Exception handling
+ */
+error_reporting(E_ALL);
+set_error_handler('Core\Error::errorHandler');
+set_exception_handler('Core\Error::exceptionHandler');
+
+
+/**
  * Routing
  */
 $router = new Core\Router();
